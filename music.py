784fc3f44e3d8a1d9d412d5ce91a5518b2f8056c
ErrrorLog <you@example.comaroslavarmen@gmail.com>
2025-04-14 14:00:29 +0500

    Добавлены анекдоты и новая музыка

--- music.py
+++ music.py
@@ -3,38 +3,23 @@ MUSIC_DB = {
         "веселое": [
             "'Всё идёт по плану' - Гражданская Оборона",
             "'Sweet Child O' Mine' - Guns N' Roses",
-            "'I Love Rock 'n' Roll' - Joan Jett",
             "'Легенда' - Кино",
-            "'Livin' on a Prayer' - Bon Jovi",
-            "'Моё сердце' - Сплин",
             "'Don’t Stop Me Now' - Queen",
-            "'Я свободен' - Кипелов",
-            "'Walk This Way' - Aerosmith",
-            "'Восьмиклассница' - Кино"
+            "'Я свободен' - Кипелов"
         ],
         "грустное": [
             "'Кукушка' - Кино",
             "'Hurt' - Nine Inch Nails",
             "'Звезда по имени Солнце' - Кино",
-            "'Creep' - Radiohead",
-            "'Мама, мы все тяжело больны' - Кино",
             "'Nothing Else Matters' - Metallica",
-            "'Грустная песня' - Сплин",
-            "'Fade to Black' - Metallica",
-            "'Где ты' - Земфира",
-            "'Tears in Heaven' - Eric Clapton"
+            "'Где ты' - Земфира"
         ],
         "энергичное": [
             "'Группа крови' - Кино",
             "'Smells Like Teen Spirit' - Nirvana",
-            "'Песня без слов' - Кино",
-            "'Sweet Dreams' - Marilyn Manson",
             "'Ветер' - ДДТ",
             "'Enter Sandman' - Metallica",
-            "'Последний герой' - Кино",
-            "'Back in Black' - AC/DC",
-            "'Это всё' - ДДТ",
-            "'Highway to Hell' - AC/DC"
+            "'Back in Black' - AC/DC"
         ]
     },
     "поп": {
@@ -42,36 +27,21 @@ MUSIC_DB = {
             "'Медуза' - Matrang",
             "'Dancing Queen' - ABBA",
             "'Танцы' - Монеточка",
-            "'Can’t Stop the Feeling!' - Justin Timberlake",
-            "'Попса' - Пропаганда",
             "'Happy' - Pharrell Williams",
-            "'Малиновый свет' - Lizer",
-            "'Vogue' - Madonna",
-            "'Половинка' - ЛСП",
-            "'La La Land' - Demi Lovato"
+            "'Vogue' - Madonna"
         ],
         "грустное": [
             "'Мам, я не хочу жить' - Монеточка",
             "'My Heart Will Go On' - Celine Dion",
-            "'Пьяная ночь' - Макс Барских",
             "'Someone Like You' - Adele",
-            "'Ты не верь слезам' - Шура",
             "'Hello' - Adele",
-            "'Мой рай' - Макс Барских",
-            "'All of Me' - John Legend",
-            "'Грустные песни' - ЛСП",
-            "'Stay' - Rihanna ft. Mikky Ekko"
+            "'All of Me' - John Legend"
         ],
         "энергичное": [
             "'Космос' - Lizer",
             "'Uptown Funk' - Mark Ronson ft. Bruno Mars",
-            "'Нон-стоп' - Reflex",
-            "'Shake It Off' - Taylor Swift",
-            "'Между нами' - LOBODA",
             "'Blinding Lights' - The Weeknd",
-            "'На заре' - Иван Дорн",
             "'Roar' - Katy Perry",
-            "'Малиновка' - МакSим",
             "'Dance Monkey' - Tones and I"
         ]
     },
@@ -79,76 +49,46 @@ MUSIC_DB = {
         "веселое": [
             "'Sing Sing Sing' - Benny Goodman",
             "'What a Wonderful World' - Louis Armstrong",
-            "'Cheek to Cheek' - Ella Fitzgerald",
-            "'Mack the Knife' - Bobby Darin",
-            "'It Don’t Mean a Thing' - Duke Ellington",
             "'Feeling Good' - Nina Simone",
             "'L-O-V-E' - Nat King Cole",
-            "'All That Jazz' - Chicago Soundtrack",
-            "'Hit the Road Jack' - Ray Charles",
-            "'I Got Rhythm' - George Gershwin"
+            "'Hit the Road Jack' - Ray Charles"
         ],
         "грустное": [
             "'My Funny Valentine' - Chet Baker",
             "'Blue in Green' - Miles Davis",
-            "'Round Midnight' - Thelonious Monk",
             "'Summertime' - Ella Fitzgerald",
-            "'God Bless the Child' - Billie Holiday",
-            "'Strange Fruit' - Billie Holiday",
-            "'Autumn Leaves' - Cannonball Adderley",
             "'Cry Me a River' - Diana Krall",
-            "'Misty' - Erroll Garner",
-            "'Body and Soul' - John Coltrane"
+            "'Misty' - Erroll Garner"
         ],
         "энергичное": [
             "'Take Five' - Dave Brubeck",
             "'So What' - Miles Davis",
-            "'A Night in Tunisia' - Dizzy Gillespie",
             "'Birdland' - Weather Report",
-            "'Spain' - Chick Corea",
-            "'Salt Peanuts' - Dizzy Gillespie",
             "'Chameleon' - Herbie Hancock",
-            "'Cantaloupe Island' - Herbie Hancock",
-            "'Caravan' - Duke Ellington",
-            "'In a Sentimental Mood' - John Coltrane"
+            "'Caravan' - Duke Ellington"
         ]
     },
     "классика": {
         "веселое": [
             "'Весна' - Вивальди",
             "'Радецки-марш' - Иоганн Штраус",
-            "'К Элизе' - Бетховен",
-            "'Венгерский танец №5' - Брамс",
-            "'Свадебный марш' - Мендельсон",
             "'Танец феи Драже' - Чайковский",
             "'Канкан' - Оффенбах",
-            "'Вальс цветов' - Чайковский",
-            "'Полет шмеля' - Римский-Корсаков",
-            "'Севильский цирюльник' - Россини"
+            "'Полет шмеля' - Римский-Корсаков"
         ],
         "грустное": [
             "'Лунная соната' - Бетховен",
             "'Реквием' - Моцарт",
             "'Адажио' - Альбинони",
-            "'Траурный марш' - Шопен",
-            "'Павана' - Форе",
             "'Лебедь' - Сен-Санс",
-            "'Ноктюрн до-диез минор' - Шопен",
-            "'Элегия' - Рахманинов",
-            "'Адажиетто' - Малер",
             "'Аве Мария' - Шуберт"
         ],
         "энергичное": [
             "'Полёт валькирий' - Вагнер",
             "'Симфония №5' - Бетховен",
-            "'Кармен-сюита' - Бизе",
-            "'Увертюра 1812' - Чайковский",
-            "'В пещере горного короля' - Григ",
             "'Болеро' - Равель",
-            "'Симфония №9' - Бетховен",
-            "'Утро' - Григ",
             "'Танец с саблями' - Хачатурян",
-            "'Финал симфонии №40' - Моцарт"
+            "'Утро' - Григ"
         ]
     },
     "электроника": {
@@ -156,37 +96,22 @@ MUSIC_DB = {
             "'Levels' - Avicii",
             "'Wake Me Up' - Avicii",
             "'Clarity' - Zedd",
-            "'Sweet Nothing' - Calvin Harris",
-            "'Don’t You Worry Child' - Swedish House Mafia",
-            "'Summertime Sadness' - Lana Del Rey (Cedric Gervais Remix)",
-            "'Call On Me' - Eric Prydz",
             "'Titanium' - David Guetta ft. Sia",
-            "'Feel So Close' - Calvin Harris",
-            "'Hey Brother' - Avicii"
+            "'Feel So Close' - Calvin Harris"
         ],
         "грустное": [
             "'Faded' - Alan Walker",
             "'Alone' - Marshmello",
-            "'The Nights' - Avicii",
-            "'Waiting for Love' - Avicii",
-            "'Scars to Your Beautiful' - Alessia Cara (Remix)",
-            "'In the Name of Love' - Martin Garrix",
             "'Silence' - Marshmello ft. Khalid",
-            "'Ocean' - Martin Garrix ft. Khalid",
             "'Wolves' - Selena Gomez & Marshmello",
             "'Let Me Go' - Hailee Steinfeld"
         ],
         "энергичное": [
             "'Animals' - Martin Garrix",
             "'Tremor' - Dimitri Vegas & Like Mike",
-            "'Turn Down for What' - DJ Snake & Lil Jon",
             "'Bangarang' - Skrillex",
-            "'Epic' - Sandro Silva & Quintino",
             "'Tsunami' - DVBBS & Borgeous",
-            "'Spaceman' - Hardwell",
-            "'Rage Valley' - Knife Party",
-            "'Mammoth' - Dimitri Vegas & Moguai",
-            "'Barricade' - Axwell"
+            "'Spaceman' - Hardwell"
         ]
     }
 }
